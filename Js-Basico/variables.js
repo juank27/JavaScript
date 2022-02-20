1c983eebb0113f12e22b3e533329f8e4b15faa9c
juank27 <juankruiz27@gmail.com>
2022-02-20 12:38:21 -0500

    variables

---
+++ Js-Basico/variables.js
@@ -0,0 +1,12 @@
+var nombre = "Juan";
+
+// declarar una variable
+var edad;
+//inicializar
+edad = 30;
+//array
+var elementos = ["lapiz", "borrador", 1];
+var persona = {
+    nombre: "Juan",
+    edad: 30,
+};
