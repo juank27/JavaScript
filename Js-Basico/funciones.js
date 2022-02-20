fd45ae574ede24c6e9168dfa4cc3902bfdbc9134
juank27 <juankruiz27@gmail.com>
2022-02-20 13:23:28 -0500

    funciones

---
+++ Js-Basico/funciones.js
@@ -0,0 +1,16 @@
+// declarativas
+function miFuncionn() {
+    return 3;
+}
+
+//Expresion
+//funciones anonimas
+var miFuncion = function(a, b) {
+    return a + b;
+}
+miFuncion(2, 4);
+
+//concatenar
+function saludar(nombre) {
+    console.log('Hola ${nombre}');
+}
