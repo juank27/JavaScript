29faa2fed1de1889692ae49d8641fe79aa0abb9e
juank27 <juankruiz27@gmail.com>
2022-03-06 20:41:40 -0500

    while y do while

--- Js-Basico/loops.js
+++ Js-Basico/loops.js
@@ -1,14 +1,25 @@
-var estudiantes = ["Maria", "Sergio","Rosa", "Daniel"]
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"]
 
 function saludarEstudiantes(estudiante) {
-	console.log(`Hola, ${estudiante}`);
+   console.log(`Hola, ${estudiante}`);
 }
 //ciclo for, igualito a java jeje
 for (var i = 0; i < estudiantes.length; i++) {
-	saludarEstudiantes(estudiantes[i])
+   saludarEstudiantes(estudiantes[i])
 }
 console.log(i);
 //for of
-for (var i of estudiantes){
-	saludarEstudiantes(i);
+for (var i of estudiantes) {
+   saludarEstudiantes(i);
 }
+
+//while
+while (estudiantes.length > 0) {
+   var estudiante = estudiantes.shift();
+   saludarEstudiantes(estudiante);
+}
+// do whiles
+do {
+   var estudiante = estudiantes.shift();
+   saludarEstudiante(estudiante);
+} while (estudiantes.length > 0);
